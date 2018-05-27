f81261ea11502468dbfe7c760c1f10b85f46e2f7
garritfra <garritfranke@gmail.com>
2018-05-27 13:38:56 +0200

    autocreate new dev environment

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "installing .vimrc"
@@ -8,5 +8,8 @@ cp ./.vimrc $HOME/.vimrc
 echo "installing .bashrc"
 cp ./.bashrc $HOME/.bashrc
 cp ./.bashrc $HOME/.bash_profile
+
+echo "creating dev workspace"
+[ -d $HOME/Documents/dev ] && echo "dev workspace already exists" || mkdir $HOME/Documents/dev echo
 
 echo "installation complete"
