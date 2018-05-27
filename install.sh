3102caba01cd9b80a47462336604e2ee997e0df3
garritfra <garritfranke@gmail.com>
2018-05-27 19:05:05 +0200

    refactor

--- install.sh
+++ install.sh
@@ -2,18 +2,22 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "✨installing .vimrc✨"
-cp ./.vimrc $HOME/.vimrc
+cp ./rcs/.vimrc $HOME/.vimrc
 
 echo "✨installing .bashrc✨"
-cp ./.bashrc $HOME/.bashrc
-cp ./.bashrc $HOME/.bash_profile
+cp ./rcs/.bashrc $HOME/.bashrc
+cp ./rcs/.bashrc $HOME/.bash_profile
 
 echo "✨creating dev workspace✨"
 [ -d $HOME/dev ] && echo "dev workspace already exists" || mkdir $HOME/dev
 
 echo "✨installing latest node & npm✨"
-curl -L https://git.io/n-install | bash
-sudo $HOME/n/bin/n stable
+source node.sh
 
-echo "🎉installation complete🎉"
-echo "❗️reload bash for the changes to take effect❗️"
+if [ $? -eq 0 ]
+then
+    echo "🎉installation complete🎉"
+    echo "❗️reload shell for the changes to take effect❗️"
+else
+    echo "Program exited with" $?
+fi
---
+++ node.sh
@@ -0,0 +1,11 @@
+node -v
+npm -v
+
+if [ $? -eq 0 ]; then  
+    echo "node & npm already installed"
+else 
+    curl -L https://git.io/n-install | bash
+    sudo $HOME/n/bin/n stable
+    sudo rm -rf $HOME/n
+fi
+
